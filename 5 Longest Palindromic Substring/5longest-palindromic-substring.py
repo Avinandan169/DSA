6e8fd4fc73a4bdfcf330ee0a6b0c95d31c968edf
Avinandan Kumar <700akak@gmail.com>
2026-04-25 05:21:25 +0000

    Code challenge submission for question: 5 Longest Palindromic Substring

---
+++ 5 Longest Palindromic Substring/5longest-palindromic-substring.py
@@ -0,0 +1,32 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ""
+        start=0; end=0
+
+        for i in range(len(s)):
+            l1=self.expand(s,i,i)
+            l2=self.expand(s,i,i+1)
+
+            max_len=max(l1,l2)
+            if max_len>(end-start):
+                start=i-(max_len-1)//2
+                end=i + max_len//2
+        return s[start:end+1]
+
+
+        
+    def expand(self,s,left,right):
+        while left>=0 and right<len(s) and s[left]==s[right]:
+            left-=1
+            right+=1
+        return right-left-1
+
+
+
+
+        
