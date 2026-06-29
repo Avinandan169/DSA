9e4125d11f546fdf3cd4befbcdc0c1b0942ac43d
Avinandan Kumar <700akak@gmail.com>
2026-06-29 10:46:17 +0000

    Code challenge submission for question: 238 Product of Array Except Self

---
+++ 238 Product of Array Except Self/238product-of-array-except-self.c
@@ -0,0 +1,22 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    int prefix[numsSize];
+    int suffix[numsSize];
+    prefix[0]=1;
+    suffix[numsSize-1]=1;
+    int* answer = (int*)malloc(numsSize * sizeof(int));
+    *returnSize=numsSize;
+    for(int i=1;i<numsSize;i++){
+        prefix[i]=prefix[i-1]*nums[i-1];
+    }
+    for(int i=numsSize-2;i>=0;i--){
+        suffix[i]=suffix[i+1]*nums[i+1];
+    }
+    for(int i=0;i<numsSize;i++){
+        answer[i]=prefix[i]*suffix[i];
+    }
+    return answer;
+
+}
