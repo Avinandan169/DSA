d210261df188cd740e7bd445c0d2f818454196dd
Avinandan Kumar <700akak@gmail.com>
2026-08-07 08:35:40 +0000

    Code challenge submission for question: 46 Permutations

---
+++ 46 Permutations/46permutations.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result=[]
+
+        def backtrack(path):
+            if(len(path)==len(nums)):
+                result.append(list(path))
+                return
+            for num in nums:
+                if num in path:
+                    continue
+            
+                path.append(num)
+                backtrack(path)
+                path.pop()
+
+        backtrack([])
+        return result
+        
+        
