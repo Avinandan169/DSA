de617e1f210df2158701c0cd35eb45d4fc48e6d9
Avinandan169 <700akak@gmail.com>
2026-09-05 22:40:47 +0530

    Time: 240 ms (96.77%) | Memory: 22.4 MB (48.39%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def firstStableIndex(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        #Avinandan169
+        n=len(nums)
+
+        suff_min=[0]*n
+        suff_min[-1]=nums[-1]
+        for i in range(n-2,-1,-1):
+            suff_min[i]=min(nums[i],suff_min[i+1])
+        
+        pref_max=nums[0]
+        for i in range(n):
+            if nums[i]>pref_max:
+                pref_max=nums[i]
+            
+            if pref_max-suff_min[i]<=k:
+                return i
+        return -1
+        
