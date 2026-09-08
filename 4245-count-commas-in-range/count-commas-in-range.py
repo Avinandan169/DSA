a6223c718d2124e48ff54e7ea096433fd4484aeb
Avinandan169 <700akak@gmail.com>
2026-09-08 13:45:12 +0530

    Time: 4 ms (18.45%) | Memory: 12.3 MB (93.20%) - LeetSync

---
+++ 4245-count-commas-in-range/count-commas-in-range.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def countCommas(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return n-1000+1 if n>=1000 else 0
+        
+
+        
+
+
+
+
+        
